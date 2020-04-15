85255b9fadcee6b1afd820d683801637dbcabeb4
faraz2000 <60105186+faraz2000@users.noreply.github.com>
2020-04-14 21:21:00 -0400

    Add files via upload

---
+++ function.js
@@ -0,0 +1,60 @@
+const getSavedNotes = function () {
+    const notesJSON = localStorage.getItem('notes')
+  
+    if (notesJSON !== null) {
+        return JSON.parse(notesJSON)
+    } else {
+        return []
+    }
+  }
+  
+  const saveNotes = function (notes) {
+    localStorage.setItem('notes', JSON.stringify(notes))
+  }
+  
+  const removeNote = function (id) {
+    const noteIndex = notes.findIndex(function (note) {
+        return note.id === id
+    })
+  
+    if (noteIndex > -1) {
+        notes.splice(noteIndex, 1)
+    }
+  }
+  
+  const generateNoteDOM = function (note) {
+    const noteEl = document.createElement('div')
+    const textEl = document.createElement('a')
+    const button = document.createElement('button')
+  
+    button.textContent = 'x'
+    noteEl.appendChild(button)
+    button.addEventListener('click', function () {
+        removeNote(note.id)
+        saveNotes(notes)
+        renderNotes(notes, filters)
+    })
+  
+    if (note.title.length > 0) {
+        textEl.textContent = note.title
+    } else {
+        textEl.textContent = 'Unnamed note'
+    }
+    textEl.setAttribute('href', `/edit.html#${note.id}`)
+    noteEl.appendChild(textEl)
+  
+    return noteEl
+  }
+  
+  const renderNotes = function (notes, filters) {
+    const filteredNotes = notes.filter(function (note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+  
+    document.querySelector('#notes').innerHTML = ''
+  
+    filteredNotes.forEach(function (note) {
+        const noteEl = generateNoteDOM(note)
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+  }
